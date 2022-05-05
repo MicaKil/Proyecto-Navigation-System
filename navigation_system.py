5384199abc7b0a40fc2c41b3b2b5bf03ca04fa8d
Micaela <89327189+MicaelaDL@users.noreply.github.com>
2022-05-05 13:55:46 -0300

    Update navigation_system.py

--- navigation_system.py
+++ navigation_system.py
@@ -1,1 +1,11 @@
+from algo1 import *
 
+# Para lograr la navegación de los elementos es necesario la creación de la flota
+#y para ello se utilizará el siguiente comando: python sistema_navegacion.py -create <local_path>
+#<local_path> representa la dirección local de la carpeta que contiene el documento con la información de las embarcaciones y su fecha correspondiente.
+
+
+def create(local_path):
+  with open(local_path,'r') as f:
+    contents = f.readlines()
+  print contents 
