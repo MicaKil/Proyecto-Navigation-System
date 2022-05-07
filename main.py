51e671deb1b605622da7a59f4e03643b19937c7b
Micaela <micaeladellongo@gmail.com>
2022-05-07 20:04:24 -0300

    commit on 'prueba-pickle-y-sys'

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from algo1 import *
 from navigation_system import *
 
-create_flotatxt(100)
+create_flotatxt(100) #crea un archivo con cien barcos
 
-create('flota.txt')
+#create('flota.txt')
--- navigation_system.py
+++ navigation_system.py
@@ -1,6 +1,7 @@
 from algo1 import *
 import random
 import pickle
+import sys
 
 # Para lograr la navegación de los elementos es necesario la creación de la flota
 #y para ello se utilizará el siguiente comando: python sistema_navegacion.py -create <local_path>
@@ -8,16 +9,44 @@ import pickle
 
 
 def create(local_path):
+  # código para probar sys (puede ser borrado)
   with open(local_path,'r') as f:
-    contents = f.readlines()
-  print(contents) 
+    flota_list = f.readlines()
+  print(flota_list) 
+
+  return #lo puse para no tener problemas con el resto del código... se puede borrar obviously
 
 
+"---------------------------------------------------------------------------------"
+"FUNCIONES SYS ARGV"
+
+# for i in range(len(sys.argv)): #cosas que hice para ver como funcionaba el módulo
+#   print (sys.argv[i])
+
+if len(sys.argv) > 1:  
+  if sys.argv[1] == "-create":
+    create(sys.argv[2]) # el argv 2 debería ser el archivo o la ubicación de este
+
+
+"---------------------------------------------------------------------------------"
+"FUNCIONES PICKLE"
+
+#copie y pegué para poder acceder a estas luego en caso de ser necesario
+
+  # with open('flota.txt', 'wb') as f: #lo serializamos
+  #   pickle.dump(flota_m,f)
+
+  # with open('flota.txt', 'rb') as f: #deserializacion
+  #   flota_d = pickle.load(f)
+
+
+
+"---------------------------------------------------------------------------------"
+"---------------------------------------------------------------------------------"
 "FUNCIONES AUXILIARES"
 
-
 # las funciones create_flotatxt y random_month no están en pseudo-python porque no va a ser usadas en el programa ppal
-# su unico propósito es genera un txt para testeo 
+# su unico propósito es generar un txt para testeo 
 def create_flotatxt(n):
   direccion = ["N","S","E","W","NE","NW","SE","SW"]
 
@@ -47,6 +76,7 @@ def create_flotatxt(n):
   with open('flota.txt', 'w') as f: #si no exite el archivo flota lo crea
     f.write('\n'.join(flota_m))
 
+# genera un mes random para la lista de los barcos
 def random_month():
   mes = random.randint(1,12)
   if mes < 10:
