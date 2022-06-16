9d9a54e09403c664d8d9fc17233a183243a7c65f
Micaela Del Longo <micaeladellongo@gmail.com>
2022-06-16 16:40:24 -0300

    Update and rename aux_test.py to test.py

--- test.py
+++ test.py
@@ -2,6 +2,13 @@ from auxiliares import *
 from algo1 import *
 import random as r
 
+# aux.create_flotatxt(10) #crea un archivo con n barcos
+
+#print(create('flota.txt'))
+#print(aux.closestPair('01'))
+
+"-------------------------------------------------------------"
+
 # a = Array(5,0)
 # for i in range(len(a)):
 #     a[i] = r.randint(0,9)
